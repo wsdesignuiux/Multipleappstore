d1186262f99b1869617845a0e7e353edbeec2443
wsdesign <info.wsdesignuiux@gmail.com>
2018-12-01 12:43:15 +0530

    Initial commit

---
+++ app/src/main/java/e/wolfsoft1/multipleappstore/MainActivity.java
@@ -0,0 +1,86 @@
+package e.wolfsoft1.multipleappstore;
+
+import android.graphics.Typeface;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import adapter.CategoryAppsAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    Typeface mTypeface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+
+
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Selections"));
+        tabLayout.addTab(tabLayout.newTab().setText("Categories"));
+
+
+        mTypeface = Typeface.createFromAsset(MainActivity.this.getAssets(), "fonts/Roboto-Medium.ttf");
+        ViewGroup vg = (ViewGroup) tabLayout.getChildAt(0);
+        int tabsCount = vg.getChildCount();
+        for (int j = 0; j < tabsCount; j++) {
+            ViewGroup vgTab = (ViewGroup) vg.getChildAt(j);
+            int tabChildsCount = vgTab.getChildCount();
+            for (int i = 0; i < tabChildsCount; i++) {
+                View tabViewChild = vgTab.getChildAt(i);
+                if (tabViewChild instanceof TextView) {
+                    ((TextView) tabViewChild).setTypeface(mTypeface, Typeface.NORMAL);
+                }
+            }
+        }
+
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        viewPager = findViewById(R.id.pager);
+        CategoryAppsAdapter adapter = new CategoryAppsAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+
+        //viewPager.setOffscreenPageLimit(2);
+
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        }
+
+    public void setCurrentTab(int i) {
+        viewPager.setCurrentItem(i);
+    }
+
+
+}
